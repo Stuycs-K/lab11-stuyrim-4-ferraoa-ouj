26bf55b6604ef2efdbee4aef6bad278e8fb96c94
Aydan Ferrao <aydanf@nycstudents.net>
2025-01-13 11:01:26 -0500

    Continued progress on JavaJester's attack

--- JavaJester.java
+++ JavaJester.java
@@ -16,8 +16,25 @@ public class JavaJester extends Adventurer {
   public void setSpecial(int n) {silliness = n;}
 
   public String attack(Adventurer other) {
+    int totalDamage = 0;
     for (int i = 0; i < 7; i++) {
       int damage = (int) (Math.random() * 3) + 1;
+      totalDamage++;
+      other.applyDamage(damage);
     }
+  }
+
+  public String support(Adventurer other) {
+    return "";
+  }
+
+  //heal or buff self
+  public String support() {
+    return "";
+  }
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public String specialAttack(Adventurer other) {
+    return ""
   }
 }
